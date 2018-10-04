05308e7096d4a565d0f0780773844b3218af781a
Gage Larsen <glarsen@aquaveo.com>
2018-10-04 10:22:02 -0600

    add building

---
+++ building/build_library.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Build library from source
+"""
+import argparse
+import os
+import re
+import subprocess
+import sys
+
+
+GENERATORS = {
+    'make': None,
+    'vs2013': 'Visual Studio 12 2013 Win64',
+    'vs2015': 'Visual Studio 14 2015 Win64',
+}
+
+
+def is_dir(_dir_name):
+    """
+    Check if the given directory is actually a directory
+
+    Args:
+        _dir_name (str): path to a directory
+
+    Returns:
+        The abs path to the given directory
+
+    Raises:
+        TypeError: If `_dir_name` is not a directory.
+    """
+    if not os.path.isdir(_dir_name):
+        msg = "{} is not a directory".format(_dir_name)
+        raise TypeError(msg)
+    else:
+        return os.path.abspath(_dir_name)
+
+
+def is_file(_file_name):
+    """
+    Check if the given file is actually a file.
+
+    Args:
+        _file_name (str): path to a file
+
+    Returns:
+        The abs path to the given file
+
+    Raises:
+        TypeError: If `_file_name` is not a file.
+    """
+    if not os.path.isfile(_file_name):
+        msg = "{} is not a file".format(_file_name)
+        raise TypeError(msg)
+    else:
+        return os.path.abspath(_file_name)
+
+
+def get_args():
+    """
+    Get arguments for test script
+
+    Returns:
+        parsed used args to be used with the run_tests function
+
+    Raises:
+        TypeError: If any of the arguments are not the correct type
+    """    
+    arguments = argparse.ArgumentParser(description="Run Conan Python tests.")
+    arguments.add_argument(
+        dest='cmake_dir', type=str, nargs='?',
+        help='location of CMakeList.txt'
+    )
+    arguments.add_argument(
+        dest='build_dir', type=str, nargs='?',
+        help='location of build files'
+    )
+    arguments.add_argument(
+        dest='profile', type=str, nargs='?',
+        help='profile to build'
+    )
+    arguments.add_argument( 
+        dest='generator', type=str, nargs='?',
+        help='files to generate. (vs2013, vs2015, or make)'
+    )
+    parsed_args = arguments.parse_args()
+
+    if not parsed_args.cmake_dir or not parsed_args.build_dir \
+            or parsed_args.profile or not parsed_args.generator:
+        parsed_args.cmake_dir = input("CMakeList.txt location [{}]:".format(
+            parsed_args.cmake_dir or '.'
+        )) or '.'
+        parsed_args.build_dir = input("build location [{}]:".format(
+            parsed_args.build_dir or '.'
+        )) or '.'
+        parsed_args.profile = input("profile [{}]:".format(
+            parsed_args.profile or '.\\default'
+        )) or '.\\default'
+        parsed_args.generator = input("generator [{}]".format(
+            parsed_args.generator or 'make'
+        )) or 'make'
+
+        parsed_args.cmake_dir = is_dir(parsed_args.cmake_dir)
+        parsed_args.build_dir = is_dir(parsed_args.build_dir)
+
+        # Profiles
+        precompile_profiles = {}
+        for root, subdir, files in os.walk('..\\profiles'):
+            for f in files:
+                precompile_profiles[f] = os.path.abspath(os.path.join(root, f))
+        if not parsed_args.profile in precompile_profiles.keys():
+            parsed_args.profile = is_file(parsed_args.profile)
+        else:
+            parsed_args.profile = precompile_profiles[parsed_args.profile]
+
+        # Generators
+        if parsed_args.generator not in GENERATORS:
+            msg = 'specified generator not supported "{}". Must be one of [{}]'.format(
+                      parsed_args.generator, ", ".join(GENERATORS.keys())
+            )
+            raise TypeError(msg)
+
+    return parsed_args
+
+
+def conan_install(_profile, _cmake_dir, _build_dir):
+    subprocess.call([
+        'conan', 'install', '-if', _build_dir,
+        '-pr', _profile, _cmake_dir
+    ])
+
+def get_cmake_options(_build_dir):
+    conan_options = {}
+    conan_option_re = r'(pybind|testing|xms){1}=(True|False)'
+    conan_info_file = os.path.join(_build_dir, 'conaninfo.txt')
+    with open(conan_info_file, 'r') as cf:
+        for line in cf.readlines():
+            o = re.search(conan_option_re, line)
+            if o:
+                conan_options[o.group(1)] = o.group(2)
+    cmake_options = []
+    cmake_options.append('-DBUILD_TESTING={}'.format(conan_options.get('testing', 'False')))
+    cmake_options.append('-DIS_PYTHON_BUILD={}'.format(conan_options.get('pybind', 'False')))
+    cmake_options.append('-DXMS_BUILD={}'.format(conan_options.get('xms', 'False')))
+    return cmake_options
+
+def run_cmake(_cmake_dir, _build_dir, _generator, _cmake_options):
+    cmd = ['cmake']
+    gen = GENERATORS[_generator]
+    if gen:
+        cmd += ['-G', '{}'.format(gen)]
+    cmd += _cmake_options
+    cmd.append(_cmake_dir)
+    os.chdir(_build_dir)
+    subprocess.run(cmd)
+
+if __name__ == "__main__":
+    args = get_args()
+    conan_install(args.profile, args.cmake_dir, args.build_dir)
+    cmake_options = get_cmake_options(args.build_dir)
+    run_cmake(args.cmake_dir, args.build_dir, args.generator, cmake_options)
